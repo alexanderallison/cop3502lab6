989d8130a99436b6adb25eae952a7202dea34a92
alexanderallison <alexande.allison@ufl.edu>
2023-07-19 20:00:06 -0400

    changed name of function encode_it() to encode() fixed some indents

--- lab6.py
+++ lab6.py
@@ -1,21 +1,22 @@
 # Alexander Allison - alexande.allison@ufl.edu
-def encode_it(original_pass_string):
-  decoded_pass_list = []
-  # convert to decoded list of ints
-  for num in original_pass_string:
-    x = int(num)
-    decoded_pass_list.append(x)
-  encoded_pass_list = []
-  for num in decoded_pass_list:
-    if num < 7:
-      encoded_pass_list.append(num + 3)
-    else: # num is > 6 (7,8,9)
-      encoded_num = (num + 3) % 10 # last digit of 2-digit number
-      encoded_pass_list.append(encoded_num)
-  encoded_string = ''
-  for num in encoded_pass_list:
-    encoded_string += str(num)
-  return encoded_string
+def encode(original_pass_string):
+    decoded_pass_list = []
+    # convert to decoded list of ints
+    for num in original_pass_string:
+        x = int(num)
+        decoded_pass_list.append(x)
+    encoded_pass_list = []
+    for num in decoded_pass_list:
+        if num < 7:
+            encoded_pass_list.append(num + 3)
+        else:  # num is > 6 (7,8,9)
+            encoded_num = (num + 3) % 10  # last digit of 2-digit number
+            encoded_pass_list.append(encoded_num)
+    encoded_string = ''
+    for num in encoded_pass_list:
+        encoded_string += str(num)
+    return encoded_string
+
 
 # Nicholas Desmornes - ndesmornes@ufl.edu
 def decode(encoded_pass):
@@ -26,6 +27,7 @@ def decode(encoded_pass):
         decoded_pass += str((int(num) + 10 - 3) % 10)
 
     return decoded_pass
+
 
 if __name__ == '__main__':
     selector = None
@@ -41,7 +43,7 @@ if __name__ == '__main__':
         selector = input('Please enter an option: ')
         if selector == '1':
             original_pass = input('Please enter your password to encode: ')
-            encoded_pass = encode_it(original_pass)
+            encoded_pass = encode(original_pass)
             print('Your password has been encoded and stored!\n')
             pass
         if selector == '2':
